82d2968abfabbe033ecbc5f76917e82a8d592b24
taschukMax <maxim.taschuk@gmail>
2014-04-04 10:05:25 +0300

    Test commit

---
+++ basics/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: maksym.tashchuk
+ * Date: 4/4/14
+ * Time: 9:58 AM
+ * To change this template use File | Settings | File Templates.
+ */
+document.write("hello there 111");
