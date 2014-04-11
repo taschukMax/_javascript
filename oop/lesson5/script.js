95b4ea8812cc0bdc4124f01522d66291edb61cab
taschukMax <maxim.taschuk@gmail>
2014-04-11 13:34:43 +0300

    Exercise 1

--- oop/lesson5/script.js
+++ oop/lesson5/script.js
@@ -1,108 +1,96 @@
-/**
- * User: maksym.tashchuk
- * Date: 4/10/14
- * Time: 3:57 PM
- */
 var oojs = (function (oojs) {
-        var createToolbarItems = function (itemElements) {
-            var items = [];
-            [].forEach.call(itemElements, function (element, index, array) {
-                    var item = {
-                        toggleActiveState: function () {
-                            this.activated = !this.activated;
-                        }
-                    };
-                    Object.defineProperties(item, {
-                        element: {
-                            value: element
-                        },
-                        enabled: {
-                            get: function () {
-                                return !this.element.classList.contains("disabled");
-                            },
-                            set: function (value) {
-                                if (value) {
-                                    this.element.classList.remove("disabled");
-                                } else {
-                                    this.element.classList.add("disabled");
-                                }
-                            }
-                        },
-                        activated: {
-                            get: function () {
-                                return this.element.classList.contains("active");
-                            },
-                            set: function (value) {
-                                if (value) {
-                                    this.element.classList.add("active");
-                                } else {
-                                    this.element.classList.remove("active");
-                                }
-                            }
-                        }
-                    });
-                    /*var item = {
-                     element: element,
-                     disabled: function () {
-                     this.element.classList.add("disabled");
-                     },
-                     enable: function () {
-                     this.element.classList.remove("disabled");
-                     },
-                     isDisabled: function () {
-                     return this.element.classList.contains("disabled");
-                     },
-                     activate: function () {
-                     if (this.isDisabled()) {
-                     return;
-                     }
-                     this.element.classList.add("active");
-                     },
-                     deactivate: function () {
-                     if (this.isDisabled()) {
-                     return;
-                     }
-                     this.element.remove("active")
-                     },
-                     isActive: function () {
-                     return this.element.classList.contains("active");
-                     },
-                     toggleActiveState: function () {
-                     if (this.isActive()) {
-                     this.deactivate();
-                     } else {
-                     this.activate();
-                     }
-                     }
-                     };*/
-                    items.push(item);
+    var createToolBarItem = function (itemElement) {
+        var item = {
+            toggleActiveState: function () {
+                this.activated = !this.activated;
+            }
+        };
+
+        Object.defineProperties(item, {
+            element: {
+                value: itemElement
+            },
+            enabled: {
+                get: function () {
+                    return !this.element.classList.contains("disabled");
+                },
+                set: function (value) {
+                    if (value) {
+                        this.element.classList.remove("disabled");
+                    } else {
+                        this.element.classList.add("disabled");
+                    }
                 }
-            )
-            ;
-            return items;
-        };
-        oojs.createToolbar = function (elementId) {
-            var element = document.getElementById(elementId);
-            if (!element) {
-                element = document.createElement("div");
-                element.id = elementId;
+            },
+            activated: {
+                get: function () {
+                    return this.element.classList.contains("active");
+                },
+                set: function (value) {
+                    if (value) {
+                        this.element.classList.add("active");
+                    } else {
+                        this.element.classList.remove("active");
+                    }
+                }
             }
-            var items = element.querySelectorAll(".toolbar-item");
-            return {
-                items: createToolbarItems(items)
+        });
+        return item;
+    };
+
+    var createToolBarItems = function (itemElements) {
+        var items = [];
+        [].forEach.call(itemElements, function (el, index, array) {
+            var item = createToolBarItem(el);
+            items.push(item);
+        });
+        return items;
+    };
+    oojs.createToolbar = function (elementId) {
+        var element = document.getElementById(elementId);
+        if (!element) {
+            element = document.createElement("div");
+            element.id = elementId;
+            element.className = "toolbar";
+        }
+        var items = element.querySelectorAll(".toolbar-item");
+        var toolbar = {
+            add: function (options) {
+                var span = document.createElement("span");
+                span.className = "toolbar-item";
+                this.el.appendChild(span);
+                var item = createToolBarItem(span);
+                this.items.push(item);
+            },
+            remove: function (index) {
+                var len = this.items.length;
+                if (index > len || index < 0) {
+                    throw new Error("Can't delete item");
+                }
+                var item = this.items[index];
+                this.items.splice(index, 1);
+                this.el.removeChild(item.el);
+            },
+            appendTo: function (parentElement) {
+                parentElement.appendChild(this.el);
             }
         };
-        return oojs;
-    }
-        (oojs || {})
-        )
-    ;
+        Object.defineProperties(toolbar, {
+            el: {
+                value: element
+            },
+            items: {
+                value: createToolBarItems(items),
+                enumerable: true
+            }
+        });
+        return toolbar;
+    };
+    return oojs;
+}(oojs || {}));
 
 
-/*var toolbar = oojs.createToolbar("myToolbar");
- var toolbarItem = toolbar.items[0];
-
- toolbarItem.setEnabled(true);
- toolbarItem.getEnabled();
- toolbarItem.enabled = true;
- var enabled = toolbarItem.enabled;*/
+var toolbar = oojs.createToolbar("myToolbar");
+toolbar.items[0].enabled = false;
+toolbar.items[2].enabled = false;
+toolbar.add();
---
+++ oop/lesson5/script_old.js
@@ -0,0 +1,39 @@
+/**
+ * User: maksym.tashchuk
+ * Date: 4/11/14
+ * Time: 11:27 AM
+ */
+/*var item = {
+ element: element,
+ disabled: function () {
+ this.element.classList.add("disabled");
+ },
+ enable: function () {
+ this.element.classList.remove("disabled");
+ },
+ isDisabled: function () {
+ return this.element.classList.contains("disabled");
+ },
+ activate: function () {
+ if (this.isDisabled()) {
+ return;
+ }
+ this.element.classList.add("active");
+ },
+ deactivate: function () {
+ if (this.isDisabled()) {
+ return;
+ }
+ this.element.remove("active")
+ },
+ isActive: function () {
+ return this.element.classList.contains("active");
+ },
+ toggleActiveState: function () {
+ if (this.isActive()) {
+ this.deactivate();
+ } else {
+ this.activate();
+ }
+ }
+ };*/
