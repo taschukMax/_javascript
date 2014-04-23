b4f8ab3a8ff57bdd8f7ae833aa59a6795ff9bbfb
taschukMax <maxim.taschuk@gmail>
2014-04-23 15:03:52 +0300

    OOP in JavaScript

--- oop/prototype/app.js
+++ oop/prototype/app.js
@@ -1,15 +1,31 @@
-/**
- * User: maksym.tashchuk
- * Date: 4/17/14
- * Time: 12:53 PM
- */
-var some_obj = {
-    name: "Ninja",
-    say: function () {
-        return "I'm a " + this.name;
+var shape = {
+    type: "Triangle",
+    getType: function () {
+        return this.type;
     }
 };
 
-function F() {
+function Triangle(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
 }
-F.prototype = some_obj;
+Triangle.prototype = shape;
+Triangle.prototype.getPerimeter = function () {
+    return this.a + this.b + this.c;
+};
+var triangle = new Triangle(1, 2, 3);
+console.log(triangle.constructor);
+/*console.log(shape.isPrototypeOf(triangle));
+ console.log(triangle.getPerimeter());
+ console.log(triangle.getType());*/
+Array.prototype.shuffle = function () {
+    return "Array";
+};
+
+String.prototype.reverse = function () {
+    return Array.prototype.reverse.apply(this.split('')).join('');
+};
+
+console.log("MAXIM".reverse());
+console.log([1, 2, 3, 4, 5, 6, 7, 8, 9].shuffle());
